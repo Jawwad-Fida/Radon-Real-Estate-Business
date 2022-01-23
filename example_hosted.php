c8b3933a0cdaf928dfc3db3cd77f5893b8dee17d
Mohammed Jawwadul Islam Fida <fidajisa@hotmail.com>
2022-01-23 12:07:34 +0600

    payment config path update

--- example_hosted.php
+++ example_hosted.php
@@ -82,7 +82,7 @@ if (is_client() == true) {
             <div class="col-md-4 order-md-2 mb-4">
                 <h4 class="d-flex justify-content-between align-items-center mb-3">
                     <span class="text-muted">Your Invoice Details</span>
-                    <span class="badge badge-secondary badge-pill">Dhur ho</span>
+                    <span class="badge badge-secondary badge-pill">Details Below</span>
                 </h4>
                 <ul class="list-group mb-3">
                     <li class="list-group-item d-flex justify-content-between lh-condensed">
